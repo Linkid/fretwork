dd26a360bb12087c3fbb80bbd2c0b5c7dc47003a
François Magimel <magimel.francois@gmail.com>
2021-01-11 20:21:26 +0100

    mora debugs and install

--- build.sh
+++ build.sh
@@ -31,12 +31,12 @@ case ${operating_system} in
         brew update --quiet > /dev/null
         brew install --quiet \
             libvorbis
-        #    portmidi \
-        #    sdl \
-        #    sdl_image \
-        #    sdl_mixer \
-        #    sdl_ttf \
-        #    wget;
+            portmidi \
+            sdl \
+            sdl_image \
+            sdl_mixer \
+            sdl_ttf \
+            wget;
         #wget https://www.surina.net/soundtouch/soundtouch-2.0.0.zip;
         #unzip soundtouch-2.0.0.zip;
         #cd soundtouch;
@@ -48,6 +48,10 @@ case ${operating_system} in
 
         brew ls libvorbis
         brew ls libogg
+        brew ls sdl
+        brew ls sdl_image
+        brew ls sdl_mixer
+        brew ls glib
         echo "which pkg-config:" `which pkg-config`
         echo "copy"
         cp -rp /usr/local/Cellar/libvorbis/1.3.7/lib/pkgconfig/* /Users/appveyor/.gvm/pkgsets/go1.15.6/global/overlay/lib/pkgconfig/
@@ -56,10 +60,8 @@ case ${operating_system} in
         echo "list pkgconfig files"
         ls -l /Users/appveyor/.gvm/pkgsets/go1.15.6/global/overlay/lib/pkgconfig
         echo "list pkgconfig files in path"
-        ls -l /Users/appveyor/.gvm/pkgsets/go1.15.6/global/overlay/lib/pkgconfig
+        ls -l /Library/Frameworks/Mono.framework/Home/bin/pkg-config
         echo "ok"
-
-        pkg-config --libs --cflags vorbisfile
     ;;
     *)
         echo "[*] no OS: " ${operating_system}
