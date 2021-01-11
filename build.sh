df3bcc37211d5f602deef84990f9028c38da79e6
François Magimel <magimel.francois@gmail.com>
2021-01-11 19:28:18 +0100

    build / macos: only libvorbis

--- build.sh
+++ build.sh
@@ -29,31 +29,30 @@ case ${operating_system} in
     "macos")
         echo "MacOS…"
         brew update --quiet > /dev/null
-        brew reinstall pkg-config
         brew install --quiet \
-            libvorbis \
-            portmidi \
-            sdl \
-            sdl_image \
-            sdl_mixer \
-            sdl_ttf \
-            wget;
-        wget https://www.surina.net/soundtouch/soundtouch-2.0.0.zip;
-        unzip soundtouch-2.0.0.zip;
-        cd soundtouch;
-        ./bootstrap;
-        ./configure;
-        make;
-        make install;
-        cd ..;
+            libvorbis
+        #    portmidi \
+        #    sdl \
+        #    sdl_image \
+        #    sdl_mixer \
+        #    sdl_ttf \
+        #    wget;
+        #wget https://www.surina.net/soundtouch/soundtouch-2.0.0.zip;
+        #unzip soundtouch-2.0.0.zip;
+        #cd soundtouch;
+        #./bootstrap;
+        #./configure;
+        #make;
+        #make install;
+        #cd ..;
 
         brew ls libvorbis
         which pkg-config
         ls -l $(which pkg-config)
-        brew unlink pkg-config && brew link pkg-config
+        #brew unlink pkg-config && brew link pkg-config
         echo $PKG_CONFIG_PATH
-        export PKG_CONFIG_PATH=/usr/local/Cellar/libvorbis/1.3.7/lib/pkgconfig:${PKG_CONFIG_PATH}
-        echo $PKG_CONFIG_PATH
+        #export PKG_CONFIG_PATH=/usr/local/Cellar/libvorbis/1.3.7/lib/pkgconfig:${PKG_CONFIG_PATH}
+        #echo $PKG_CONFIG_PATH
         pkg-config --libs --cflags vorbisfile
     ;;
     *)
